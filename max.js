da494e854bae6bc81635fd075f86130173fc7b20
Jonathan Moore <jmjonemoore@gmail.com>
2019-04-30 11:23:51 -0500

    Adding Max and Min

---
+++ max.js
@@ -0,0 +1,27 @@
+function max(num){
+ let result = num[0];
+ let i = 0;
+ while(i < num.length){
+     if(num[i] > result){
+         result = num[i];
+     }
+     i++;
+}    
+return result;
+}
+
+function min(num){
+    let result = num[0];
+    let i = 0;
+    while(i < num.length){
+        if(num[i] < result){
+            result = num[i];
+        }
+        i++;
+    }
+    return result;
+}
+
+let arr = [0,1,2,3,7,25];
+console.log(max(arr));
+console.log(min(arr));
